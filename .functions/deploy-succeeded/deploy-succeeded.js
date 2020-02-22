bf96a15cf1b2f6586db556749fcb413f5e79f7cd
Raymond Camden <raymondcamden@gmail.com>
2020-02-22 08:04:18 -0600

    big test

--- .functions/deploy-succeeded/deploy-succeeded.js
+++ .functions/deploy-succeeded/deploy-succeeded.js
@@ -1,4 +1,22 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const SDK = require('@ringcentral/sdk').SDK;
+
+RECIPIENT = process.env.SMS_RECIP;
+
+RINGCENTRAL_CLIENTID = process.env.RINGCENTRAL_CLIENTID;
+RINGCENTRAL_CLIENTSECRET = process.env.RINGCENTRAL_CLIENTSECRET;
+RINGCENTRAL_SERVER = process.env.RINGCENTRAL_SERVER;
+
+RINGCENTRAL_USERNAME = process.env.RINGCENTRAL_USERNAME;
+RINGCENTRAL_PASSWORD = process.env.RINGCENTRAL_PASSWORD;
+RINGCENTRAL_EXTENSION = process.env.RINGCENTRAL_EXTENSION;
+
+var rcsdk = new SDK({
+    server: RINGCENTRAL_SERVER,
+    clientId: RINGCENTRAL_CLIENTID,
+    clientSecret: RINGCENTRAL_CLIENTSECRET
+});
+var platform = rcsdk.platform();
+
 exports.handler = async (event, context) => {
   try {
 
@@ -11,6 +29,7 @@ exports.handler = async (event, context) => {
     let buildDuration = pubData.deploy_time;
 
     console.log(`BUILT at ${buildTime} in ${buildDuration} seconds`);
+    await sendSMS(buildTime, buildDuration);
 
     return {
       statusCode: 200,
@@ -19,4 +38,25 @@ exports.handler = async (event, context) => {
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
+}
+
+async function sendSMS(time,duration) {
+
+  console.log('do login');
+	await platform.login({
+		username: RINGCENTRAL_USERNAME,
+		password: RINGCENTRAL_PASSWORD,
+		extension: RINGCENTRAL_EXTENSION
+		});
+	console.log('done logging in');
+	let resp = await platform.post('/restapi/v1.0/account/~/extension/~/sms', {
+		from: {'phoneNumber': RINGCENTRAL_USERNAME},
+		to: [{'phoneNumber': RECIPIENT}],
+		text: `Site built at ${time} and took ${duration} seconds.`
+  });
+	
+	console.log('done with msg');
+  let data = await resp.json();
+  return data;
+
 }
