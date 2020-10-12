f66b37dca62b9188c2d88d568e5afb302f0c6174
Raymond Camden <raymondcamden@gmail.com>
2020-10-12 16:30:41 -0500

    testing

--- .functions/submission-created/submission-created.js
+++ .functions/submission-created/submission-created.js
@@ -1,36 +1,44 @@
-// // optionally configure local env vars
-// require('dotenv').config()
+const SG_KEY = process.env.SENDGRID;
 
-// // details in https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget
-const fetch = require('node-fetch')
-const { EMAIL_TOKEN } = process.env
+const helper = require('sendgrid').mail;
+const sg = require('sendgrid')(SG_KEY);
+
 exports.handler = async event => {
+
   const form = JSON.parse(event.body).payload;
-
   console.log(`Recieved a submission: ${JSON.stringify(form)}`);
-  /*
-  return fetch('https://api.buttondown.email/v1/subscribers', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Token ${EMAIL_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(`Submitted to Buttondown:\n ${data}`)
-    })
-    .catch(error => ({ statusCode: 422, body: String(error) }))
-    */
 
-  let result = {};
-  return {
-    statusCode: 200,
-    headers : {
-      'Content-Type':'application/json'
-    },
-    body: JSON.stringify(result)
-  }
+
+  let from_email = new helper.Email('raymondcamden@gmail.com');
+	let to_email = new helper.Email('raymondcamden@gmail.com');
+	let subject = 'Form Submission';
+
+  let content = `Form:
+${JSON.stringify(form,null, '\t')}`;
+
+  let mailContent = new helper.Content('text/plain', content);
+	let mail = new helper.Mail(from_email, subject, to_email, mailContent);
+
+	let request = sg.emptyRequest({
+		method: 'POST',
+		path: '/v3/mail/send',
+		body: mail.toJSON()
+	});
+
+  sg.API(request, function(error, response) {
+		if(error) {
+			console.log(error.response.body);
+		} else {
+      return {
+        statusCode: 200,
+        headers : {
+          'Content-Type':'application/json'
+        },
+        body: { success: true } 
+      }
+
+    }
+	});
+
 
 }
